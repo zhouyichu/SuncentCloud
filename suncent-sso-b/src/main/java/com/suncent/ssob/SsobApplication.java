1349f9eee9a13c1bd10e54f79b083fe19c52a3eb
zhoufei <zhou.hq817@outlook.com>
2020-11-02 18:32:17 +0800

    init object

---
+++ suncent-sso-b/src/main/java/com/suncent/ssob/SsobApplication.java
@@ -0,0 +1,13 @@
+package com.suncent.ssob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsobApplication.class, args);
+	}
+
+}
